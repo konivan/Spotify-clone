3adb0cf32620bc5a08aa79d0b420f8fbd1101011
konivan <konivan08@gmail.com>
2022-07-16 14:14:50 +0300

    connect to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { TrackModule } from "./track/track.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 
 @Module({
-  imports: [TrackModule],
+  imports: [TrackModule, MongooseModule.forRoot('mongodb+srv://konivan:konivan@cluster0.fvtap54.mongodb.net/?retryWrites=true&w=majority')],
 })
 export class AppModule {}
