d59dc3ea08c38240f0df120bcaa665c3dca2ec53
konivan <konivan08@gmail.com>
2022-07-16 13:19:47 +0300

    first commit

---
+++ src/track/track.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable()
+export class TrackService {
+
+  async create() {
+
+  }
+
+  async getAll() {
+    return 'get all users'
+  }
+
+  async getOne() {
+
+  }
+
+  async delete() {
+
+  }
+}
