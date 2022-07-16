e8d0ba3ea53f65be400fc00ab9054c21fc4a4d27
konivan <konivan08@gmail.com>
2022-07-16 14:27:48 +0300

    inject models

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,9 +1,16 @@
+import { Comment, CommentSchema } from './schemas/comment.schema';
+import { TrackSchema, Track } from './schemas/track.schema';
 import { TrackController } from './track.controller';
 import { Module } from '@nestjs/common';
 import { TrackService } from './track.service';
+import { MongooseModule } from "@nestjs/mongoose";
 
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name: Track.name, schema: TrackSchema}]),
+    MongooseModule.forFeature([{name: Comment.name, schema: CommentSchema}])
+  ],
   controllers: [TrackController],
   providers: [TrackService]
 })
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,22 +1,24 @@
+import { Comment, CommentDocument } from './schemas/comment.schema';
+import { Track, TrackDocument } from './schemas/track.schema';
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from 'mongoose';
 
 
 @Injectable()
 export class TrackService {
+  constructor(
+    @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+    @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+  ) {}
 
-  async create() {
-
-  }
+  async create() {}
 
   async getAll() {
-    return 'get all users'
+    return 'get all users';
   }
 
-  async getOne() {
+  async getOne() {}
 
-  }
-
-  async delete() {
-
-  }
+  async delete() {}
 }
